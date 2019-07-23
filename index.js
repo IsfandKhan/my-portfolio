0f6f5a9c07e8d3788365ddcbbd58447a7dfca16b
Isfand Yar Khan <ikhan@ssidecisions.com>
2019-07-23 10:05:21 +0500

    Added Project Pages and main.handlebars file

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var path = require('path');
 var app = express();
 var server = require('http').createServer(app);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.set('views', __dirname);
-app.engine('handlebars', exphbs({defaultLayout:'index'}));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-    res.render('layouts/index');
+    res.render('index');
 });
 
 server.listen(process.env.PORT || 3000);
