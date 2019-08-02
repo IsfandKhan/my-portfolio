4d68f26e157bce6cf1008c09c440f7d01fc8ff91
Isfand Yar Khan <ikhan@ssidecisions.com>
2019-08-02 11:15:00 +0500

    Changed Architecture to MVC

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var path = require('path');
+
 var app = express();
-var server = require('http').createServer(app);
+
+var IndexRouter = require('./routes/index');
+app.use('/', IndexRouter);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res){
     res.render('index');
 });
 
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
