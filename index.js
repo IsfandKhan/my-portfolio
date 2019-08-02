28a2b6bf403e869fc4872728ed0ad61db4aa8327
Isfand Yar Khan <ikhan@ssidecisions.com>
2019-08-02 14:40:34 +0500

    Added github

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var path = require('path');
 var app = express();
 
 var IndexRouter = require('./routes/index');
-app.use('/', IndexRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
@@ -14,8 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-    res.render('index');
-});
+app.use('/', IndexRouter);
 
 app.listen(process.env.PORT || 3000);
